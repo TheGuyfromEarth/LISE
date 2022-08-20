2ecdb5992a2dd57201d0c2a8eda855dc31d4b0f2
TheGuyfromEarth <91563278+TheGuyfromEarth@users.noreply.github.com>
2022-08-20 23:13:38 +0530

    linked list question solutions

--- DSA2/src/linkedList/OddEvenLinkedList.java
+++ DSA2/src/linkedList/OddEvenLinkedList.java
@@ -7,4 +7,50 @@ package linkedList;
 // O/P -> 9 - 3 - 9 - 5 - 91 - 4 - 8 - 14 - 10  - 34
 
 public class OddEvenLinkedList {
+    public static void main(String[] args) {
+        MyLinkedList list = new MyLinkedList();
+        list.addLast(20);
+        list.addLast(4);
+        list.addLast(31);
+        list.addLast(8);
+        list.addLast(90);
+        list.addLast(14);
+        list.addLast(19);
+        list.addLast(56);
+        list.addLast(34);
+        list.addLast(92);
+        MyLinkedList resList = oddEvenList(list);
+        resList.traverse();
+    }
+
+    private static MyLinkedList oddEvenList(MyLinkedList list){
+        MyLinkedList oddList = new MyLinkedList();
+        MyLinkedList evenList = new MyLinkedList();
+
+        Node temp = list.head;
+
+        while (temp!=null){
+            int val = temp.data;
+            list.removeFirst();
+
+            if(val%2==0)
+                evenList.addLast(val);
+            else
+                oddList.addLast(val);
+
+            temp=temp.next;
+        }
+
+        // connect odd list's tail with even list's head and update tail
+
+        if(oddList.size == 0 && evenList.size!=0){
+            return evenList;
+        }else if(oddList.size != 0 && evenList.size == 0)
+            return oddList;
+        else {
+            oddList.tail.next = evenList.head;
+            oddList.tail = evenList.tail;
+        }
+        return oddList;
+    }
 }
--- DSA2/src/linkedList/RemovingDuplicatesFromSortedLinkedList.java
+++ DSA2/src/linkedList/RemovingDuplicatesFromSortedLinkedList.java
@@ -3,4 +3,39 @@ package linkedList;
 // Ex: list ->   3 -  4  - 4 - 9 - 9 - 14 - 34 - 34 - 34 - 90
 // O/P -> 3 - 4 - 9 - 14 - 34 - 90
 public class RemovingDuplicatesFromSortedLinkedList {
+
+    public static void main(String[] args) {
+        MyLinkedList list = new MyLinkedList();
+        list.addLast(3);
+        list.addLast(4);
+        list.addLast(4);
+        list.addLast(9);
+        list.addLast(9);
+        list.addLast(14);
+        list.addLast(34);
+        list.addLast(34);
+        list.addLast(34);
+        list.addLast(90);
+        MyLinkedList newList = removeDuplicates(list);
+        newList.traverse();
+    }
+
+    private static MyLinkedList removeDuplicates(MyLinkedList list) {
+        Node curr = list.head;
+        MyLinkedList newList = new MyLinkedList();
+
+        while (curr != null) {
+            int val = curr.data;
+            curr = curr.next;
+            list.removeFirst();
+
+            if(newList.size == 0)
+                newList.addLast(val);
+            else {
+                if (newList.tail.data != val)
+                    newList.addLast(val);
+            }
+        }
+        return newList;
+    }
 }
