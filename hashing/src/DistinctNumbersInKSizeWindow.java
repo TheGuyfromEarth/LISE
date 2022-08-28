1a09a2cea763fbb7dd8bfa0fc61c48a1fd31facd
TheGuyfromEarth <91563278+TheGuyfromEarth@users.noreply.github.com>
2022-08-28 23:00:06 +0530

    hashing questions

--- hashing/src/DistinctNumbersInKSizeWindow.java
+++ hashing/src/DistinctNumbersInKSizeWindow.java
@@ -2,5 +2,44 @@
 // k = 3
 // O/P -> 2,2,3,2,3,3
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DistinctNumbersInKSizeWindow {
+    public static void main(String[] args) {
+        int[] arr = {9,2,2,6,5,6,4,9};
+        int k = 3;
+        System.out.println(distinctNumbers(arr,k));
+    }
+
+    public static List<Integer> distinctNumbers(int[] arr, int k){
+        Map<Integer,Integer> freqMap = new HashMap<>();
+        ArrayList<Integer> resList = new ArrayList<>();
+        // put first k numbers inside the freqMap using the acquire pointer
+        int acquire = 0;
+        int release = -1;
+
+        for(;acquire<k;acquire++){
+            freqMap.put(arr[acquire], freqMap.getOrDefault(arr[acquire],0)+1);
+        }
+
+        resList.add(freqMap.size());
+
+        for(;acquire<arr.length;acquire++){
+            // release and remove from freqMap
+            release++;
+            int val = freqMap.get(arr[release]);
+            if(val == 1)
+                freqMap.remove(arr[release]);
+            else{
+                freqMap.put(arr[release],val-1);
+            }
+            // acquire and put in freqMap
+            freqMap.put(arr[acquire], freqMap.getOrDefault(arr[acquire],0)+1);
+            resList.add(freqMap.size());
+        }
+        return resList;
+    }
 }
--- hashing/src/LongestSequence.java
+++ hashing/src/LongestSequence.java
@@ -1,5 +1,50 @@
 // 2,5,3,10,8,11,1,20,6
 // O/P -> 1,2,3
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSequence {
+    public static void main(String[] args) {
+        int[] arr = {2,5,3,10,8,11,1,20,6,4};
+        System.out.println(getLongestSequence(arr));
+    }
+
+    public static ArrayList<Integer> getLongestSequence(int[] arr){
+
+        Map<Integer,Boolean> numMap = new HashMap<>();
+        ArrayList<Integer> resList =  new ArrayList<>();
+        // put all the elements from the array with value as true by default
+        for(int i: arr)
+            numMap.put(i,true);
+
+        for(int i: arr){
+            if(numMap.containsKey(i-1))
+                numMap.put(i,false);
+        }
+
+        int maxSeqStPoint = Integer.MIN_VALUE;
+        int maxSeqSize = 0;
+
+        for(int i:arr){
+            if(numMap.get(i)==true){
+                int stPoint = i;
+                int size = 1;
+                while (numMap.containsKey(stPoint+1)){
+                    size++;
+                    stPoint++;
+                }
+                if(maxSeqSize<size) {
+                    maxSeqSize = size;
+                    maxSeqStPoint = i;
+                }
+            }
+        }
+
+        for(int i=maxSeqStPoint;i<=maxSeqSize;i++){
+            resList.add(i);
+        }
+        return resList;
+    }
 }
