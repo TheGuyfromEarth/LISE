829923af3312cc2675cc65b13cd1d9a94b8c3845
TheGuyfromEarth <rishabhgupta01994@gmail.com>
2022-11-29 23:03:44 +0530

    Graphs DFS Questions

--- dsa3/src/graphs/AllPaths.java
+++ dsa3/src/graphs/AllPaths.java
@@ -3,5 +3,49 @@ package graphs;
 // Given a graph, and a source and destination vertex, print all the possible paths
 // between the two vertices.
 
+import java.util.Scanner;
+
 public class AllPaths {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("The number of vertices: ");
+        int numVertices = input.nextInt();
+        Graph graph = new Graph(numVertices);
+
+        System.out.println("Enter number of edges: ");
+        int numEdges = input.nextInt();
+
+        for (int edge = 0; edge < numEdges; edge++) {
+            int source = input.nextInt();
+            int destination = input.nextInt();
+            graph.connectEdges(source, destination);
+        }
+
+        // graph with nodes and desired edges has been created
+
+        System.out.println("Enter the source vertex: ");
+        int srcVertex = input.nextInt();
+
+        System.out.println("Enter the destination vertex: ");
+        int destVertex = input.nextInt();
+
+        // allPaths.....
+        boolean[] visited = new boolean[numVertices];
+        printAllPaths(graph, srcVertex, destVertex, visited, ""+srcVertex);
+    }
+
+    public static void printAllPaths(Graph graph, int srcVertex, int destVertex, boolean[] visited, String path) {
+        if (srcVertex == destVertex) {
+            // print path
+            System.out.println(path);
+            return;
+        }
+        visited[srcVertex] = true;
+        for (int neighbour : graph.adjacencyList[srcVertex]) {
+            if (!visited[neighbour]) {
+                printAllPaths(graph, neighbour, destVertex, visited,path+" "+neighbour);
+            }
+        }
+        visited[srcVertex] = false;
+    }
 }
--- dsa3/src/graphs/NumberOfConnectedComponents.java
+++ dsa3/src/graphs/NumberOfConnectedComponents.java
@@ -2,5 +2,51 @@ package graphs;
 
 // In the given graph, return the count of number of connected components.
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class NumberOfConnectedComponents {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("The number of vertices: ");
+        int numVertices = input.nextInt();
+        Graph graph = new Graph(numVertices);
+
+        System.out.println("Enter number of edges: ");
+        int numEdges = input.nextInt();
+
+        for (int edge = 0; edge < numEdges; edge++) {
+            int source = input.nextInt();
+            int destination = input.nextInt();
+            graph.connectEdges(source, destination);
+        }
+
+        // graph with nodes and desired edges has been created
+
+        // count connected components
+        boolean[] visited = new boolean[numVertices];
+        System.out.println(countConnectedComponents(graph, visited));
+    }
+
+    public static int countConnectedComponents(Graph graph, boolean[] visited){
+        // perform DFS for all nodes
+        int numConnectedComponents = 0;
+        for(int node=0;node<graph.adjacencyList.length;node++){
+            if(!visited[node]) {
+                numConnectedComponents++;
+                dfsUtil(graph, visited, node);
+            }
+        }
+        return numConnectedComponents;
+    }
+
+    private static void dfsUtil(Graph graph, boolean[] visited, int srcVertex){
+        visited[srcVertex] = true;
+
+        ArrayList<Integer> arrList = graph.adjacencyList[srcVertex];
+        for(int neighbour: arrList){
+            if(!visited[neighbour])
+                dfsUtil(graph, visited, neighbour);
+        }
+    }
 }
