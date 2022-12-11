1922e5fa47e17905de0a22f979d815e6671f52c9
TheGuyfromEarth <rishabhgupta01994@gmail.com>
2022-12-11 22:56:16 +0530

    Floyd Warshall and DijkstrasAlgorithm

---
+++ dsa3/src/graphs/DijkstrasAlgorithm.java
@@ -0,0 +1,71 @@
+package graphs;
+
+import java.util.*;
+
+public class DijkstrasAlgorithm {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("The number of vertices: ");
+        int numVertices = input.nextInt();
+        WeightedGraph graph = new WeightedGraph(numVertices);
+
+        System.out.println("Enter number of edges: ");
+        int numEdges = input.nextInt();
+
+        List<WtPair> wtPairList = new ArrayList<>();
+
+        for (int edge = 0; edge < numEdges; edge++) {
+            int source = input.nextInt();
+            int destination = input.nextInt();
+            int weight = input.nextInt();
+            wtPairList.add(new WtPair(source, destination, weight));
+            graph.connectEdges(source, destination, weight);
+        }
+
+        // graph with nodes and desired edges having respective weights has been created
+        System.out.println("Enter the source vertex: ");
+        int srcVertex = input.nextInt();
+        int[] dist = dijkstrasAlgorithm(graph, srcVertex);
+        System.out.println(Arrays.toString(dist));
+    }
+
+    // time complexity -> O(|V| + |E|log|V|)
+    public static int[] dijkstrasAlgorithm(WeightedGraph graph, int sourceVertex) {
+        boolean[] visited = new boolean[graph.adjacencyList.length];
+        int[] dist = new int[visited.length];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        PriorityQueue<WtPair> pq = new PriorityQueue<>();
+        WtPair sourcePair = new WtPair(sourceVertex, sourceVertex, 0);
+        dist[sourceVertex] = 0; // dist of source vertex with respect to itself will be always 0
+        pq.add(sourcePair);
+
+        while (!pq.isEmpty()) {
+
+            WtPair currPair = pq.poll();
+
+            if (visited[currPair.neighbourVertex])
+                continue;
+
+            visited[currPair.neighbourVertex] = true;
+
+            for (WtPair neighbourPair : graph.adjacencyList[currPair.neighbourVertex]) {
+                if (!visited[neighbourPair.neighbourVertex]) {
+                    // Minimization of distance
+                    if (dist[neighbourPair.neighbourVertex] > dist[neighbourPair.sourceVertex] + neighbourPair.weight)
+                        dist[neighbourPair.neighbourVertex] = dist[neighbourPair.sourceVertex] + neighbourPair.weight;
+                    pq.add(neighbourPair);
+                }
+            }
+        }
+        return dist;
+    }
+}
+// source -> 0
+// nbr -> 3
+// intermediate -> 1
+
+// 0 - 3 => infinity
+// 0 - 1 => 10
+// 1 - 3 => 5(wt)
---
+++ dsa3/src/graphs/FloydWarshallAlgorithm.java
@@ -0,0 +1,72 @@
+package graphs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// All Pair Shortest distance Algorithm
+public class FloydWarshallAlgorithm {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("The number of vertices: ");
+        int numVertices = input.nextInt();
+        WeightedGraph graph = new WeightedGraph(numVertices);
+
+        System.out.println("Enter number of edges: ");
+        int numEdges = input.nextInt();
+
+        List<WtPair> wtPairList = new ArrayList<>();
+
+        for (int edge = 0; edge < numEdges; edge++) {
+            int source = input.nextInt();
+            int destination = input.nextInt();
+            int weight = input.nextInt();
+            wtPairList.add(new WtPair(source, destination, weight));
+            graph.connectEdges(source, destination, weight);
+        }
+
+        // graph with nodes and desired edges having respective weights has been created
+        int[][] dpDist = floydWarsallAlgorithm(graph);
+        for (int row = 0; row < dpDist.length; row++) {
+            for (int col = 0; col < dpDist.length; col++) {
+                System.out.print(dpDist[row][col]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    // time complexity = O(V^3)
+    public static int[][] floydWarsallAlgorithm(WeightedGraph graph) {
+        int[][] dpDist = new int[graph.adjacencyList.length][graph.adjacencyList.length];
+
+        for (int row = 0; row < dpDist.length; row++) {
+            for (int col = 0; col < dpDist.length; col++) {
+                if(row == col) // self vertex
+                    dpDist[row][col] = 0;
+                else
+                    dpDist[row][col] = Integer.MAX_VALUE; // marking with infinity
+            }
+        }
+
+        for (int index = 0; index < graph.adjacencyList.length; index++) {
+            // mark distance dp for direct edges
+            for (WtPair neighbourPair : graph.adjacencyList[index]) {
+                dpDist[neighbourPair.sourceVertex][neighbourPair.neighbourVertex] = neighbourPair.weight;
+            }
+        }
+
+        // for indirect edges weight minimization
+        for (int k = 0; k < graph.adjacencyList.length; k++) {
+            for (int row = 0; row < dpDist.length; row++) {
+                for (int col = 0; col < dpDist.length; col++) {
+                    if(row == k || col == k)
+                        continue;
+                    if(dpDist[row][k]!=Integer.MAX_VALUE && dpDist[k][col]!=Integer.MAX_VALUE)
+                        dpDist[row][col] = Math.min(dpDist[row][col], dpDist[row][k] + dpDist[k][col]);
+                }
+            }
+        }
+        return dpDist;
+    }
+}
